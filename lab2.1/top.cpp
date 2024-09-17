ed9cebf869bc6447b5da1244e8c5363e313d6863
arschlen <arsnchld08@gmail.com>
2024-09-17 22:53:46 +0300

    arschlen

---
+++ lab2.1/top.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	double m; // вхідний параметр
+	double n; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "m = "; cin >> m;
+	cout << "n = "; cin >> n;
+	z1 = ((sqrt((3*m+2)*(3*m+2)-24*m))/(3*sqrt(m)-2/sqrt(m)));
+	z2 = sqrt(m);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
